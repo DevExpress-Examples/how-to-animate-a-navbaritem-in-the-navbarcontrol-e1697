4327e8f850c190a6ba065a173b8b661ee2dc4422
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DemoNavBar/Window1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Markup;
+
+namespace DemoNavBar {
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window {
+        public Window1() {
+            InitializeComponent();
+        }
+    }
+
+    public class AnimationProgressToAngleConverter : MarkupExtension, IValueConverter {
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            double val = (double)value;
+            return 90 * (1 - val);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to animate a NavBarItem in the NavBarControl
+
+
+<p>The example shows how to implement an animated NavBarItem. The image is changed when the mouse is hovering over a non-selected NavBarItem. This functionality is based on using NavBarControl templates.</p>
+
+<br/>
+
+
